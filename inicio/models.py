5d57ee56d43b160bb2b2b99fe1d3570325eadefc
Paola <pgelvez1@udi.edu.co>
2025-11-24 00:24:50 -0500

    ultimas mejoras

--- inicio/models.py
+++ inicio/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# CLASE DE POO: PRUEBA ACADÉMICA
+class Proyecto(models.Model):
+    """Clase que usa POO (Clases y Herencia) para definir la estructura de un proyecto."""
+    titulo = models.CharField(max_length=100)
+    tecnologia_inicial = models.CharField(max_length=50) 
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.titulo
